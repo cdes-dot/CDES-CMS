98edfdcb49fd5c3d94127203e69b136afb7290ee
Nelcido Rafael Diaz Delgado <nelcidodiaz@Nelcidos-MacBook-Air.local>
2025-08-22 01:22:55 -0400

    Agregando planes estrategicos

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -961,6 +961,10 @@ export interface ApiPlanesEstrategicoPlanesEstrategico
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    eje_estrategicos: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::eje-estrategico.eje-estrategico'
+    >;
     Locacion: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
